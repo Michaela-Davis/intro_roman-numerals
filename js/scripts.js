7897f95198cd34dd54c8844fc9aed3b5c7a915a1
Ash Laidlaw and Michaela Davis <michaela@>
2017-01-19 14:20:44 -0800

    seperate business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,28 @@
-//Symbol  Value
- // I       1
- // V       5
- // X       10
- // L       50
- // C       100
- // D       500
- // M       1,000
+// BUSINESS LOGIC
+function convert(input) {
+  var roman_numerals = ["M", "CM", "D", "CD", "C", "XL", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var output = "";
 
+  if (input > 3999 || input < 0) {
+    alert("Please enter a value between 1 and 3999")
+  }
+
+  for (var i = 0; i <= numbers.length; i++) {
+    while (input % numbers[i] < input) {
+      output += roman_numerals[i];
+      input -= numbers[i];
+    }
+  }
+  return output;
+}
+
+// USER LOGIC
 $(document).ready(function() {
   $("form.roman").submit(function(event){
     event.preventDefault();
-
     var input = parseInt($("#user_input").val());
-    var roman_numerals = ["M", "CM", "D", "CD", "C", "XL", "L", "XL", "X", "IX", "V", "IV", "I"];
-    var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    var output = "";
-
-    if (input > 3999 || input < 0) {
-      alert("Please enter a value between 1 and 3999")
-    }
-
-    for (var i = 0; i <= numbers.length; i++) {
-      while (input % numbers[i] < input) {
-        output += roman_numerals[i];
-        input -= numbers[i];
-      }
-    }
+    var output = convert(input);
 
     $("#output").text(output);
   });
